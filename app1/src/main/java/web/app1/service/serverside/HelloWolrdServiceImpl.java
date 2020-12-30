e5c0517bdfb1d87f38ffa1ed8c9b746bdfadfe9b
muneendra <muneendra.lala@outlook.com>
2020-12-30 15:08:28 +0530

    second commit

--- app1/src/main/java/web/app1/service/serverside/HelloWolrdServiceImpl.java
+++ app1/src/main/java/web/app1/service/serverside/HelloWolrdServiceImpl.java
@@ -7,7 +7,7 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
+ ****************************************/
 package web.app1.service.serverside;
 
 import java.util.Date;
